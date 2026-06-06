1ef448dcf125d582fce5f5b68c2e17beeb565278
Koji Ishii <kojii@chromium.org>
2026-06-07 00:39:00 +0900

    refactor: rename to `volume.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,16 @@ mod display;
 #[cfg(windows)]
 mod drive_path;
 #[cfg(windows)]
-mod drives;
-#[cfg(windows)]
 mod path_ext;
 mod simple_unc;
+#[cfg(windows)]
+mod volume;
 
 pub use display::Display;
 #[cfg(windows)]
 pub(crate) use drive_path::DrivePath;
 #[cfg(windows)]
-pub(crate) use drives::Drives;
-#[cfg(windows)]
 pub(crate) use path_ext::PathExt;
 pub use simple_unc::SimpleUnc;
+#[cfg(windows)]
+pub(crate) use volume::Drives;
